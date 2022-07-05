c22b65e16ebb9793dfb8903aad58cd828a142bcc
Alexander Ivkin <a@ivkin.dev>
2022-07-05 18:15:47 -0400

    Curve fitted y as well

--- curve.py
+++ curve.py
@@ -13,8 +13,8 @@ def vx(t, a, v0):
     return values.ravel()
 
 
-def x(t, a, v0):
-    return vx(t, a, v0) * t
+def x(t, a, v0, x0):
+    return vx(t, a, v0) * t + x0
 
 
 def dvy(v, t, a, g):
@@ -26,18 +26,18 @@ def vy(t, a, g, v0):
     return values.ravel()
 
 
-def y(t, a, g, v0):
-    return vy(t, a, g, v0) * t
+def y(t, a, g, v0, y0):
+    return vy(t, a, g, v0) * t + y0
 
 
 data_t = np.array([0, 1, 2, 3, 4])
 data_x = np.array([0, 10, 15, 18, 19])
 data_y = np.array([1, 5, 7, 7, 6])
 
-vals_x, _ = curve_fit(x, data_t, data_x, [-2, 10])
-vals_y, _ = curve_fit(y, data_t, data_y, [-2, -10, 10])
-ax, v0x = vals_x
-ay, g, v0y = vals_y
+vals_x, _ = curve_fit(x, data_t, data_x, [-0.2, 10, 0])
+vals_y, _ = curve_fit(y, data_t, data_y, [-0.2, -10, 10, 1])
+ax, v0x, x0 = vals_x
+ay, g, v0y, y0 = vals_y
 print('ax: {} v0: {} ay: {}, g: {}, v0y: {}'.format(ax, v0x, ay, g, v0y))
 
 t = np.linspace(0, 4, 20)
@@ -49,6 +49,6 @@ plt.ylabel("y")
 plt.title("x'' = ax * (x')^2, y'' = ay * (y')^2 - g")
 plt.plot(
     data_x, data_y, '.',
-    x(t, ax, v0x), y(t, ay, g, v0x), '-'
+    x(t, ax, v0x, x0), y(t, ay, g, v0y, y0), '-'
 )
 plt.show()
