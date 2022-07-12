b42255b73ad39ea6de4ad787ee71e251c13a1dc9
Alexander Ivkin <a@ivkin.dev>
2022-07-12 18:30:06 -0400

    3 curves

--- fit_curve.py
+++ fit_curve.py
@@ -31,11 +31,15 @@ track2_landing = np.array([
     [403.93, 474.01],
 ])
 
+track1_i = np.arange(start=0, stop=len(track), step=1)
+track1_t = np.array(list(map(lambda i: i / 30, track1_i)))
+
 track2_i = np.arange(start=0, stop=len(track2), step=1)
 track2_t = np.array(list(map(lambda i: i / 30, track2_i)))
 
 track2_landing_i = np.append(np.arange(start=0, stop=len(track2_landing) - 1, step=1), [48])
 track2_landing_t = np.array(list(map(lambda i: i / 30, track2_landing_i)))
 
+fit_2d(track, track1_t)
 fit_2d(track2_landing, track2_landing_t, 50/30)
 fit_2d(track2, track2_t)
