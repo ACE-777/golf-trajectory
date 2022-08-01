1d2c1333b841d9bf55bc3cf5df000fc0b29095d2
Alexander Ivkin <a@ivkin.dev>
2022-08-01 11:54:23 -0400

    Added chart

--- test_annotations.py
+++ test_annotations.py
@@ -2,7 +2,7 @@ import numpy as np
 import sys
 import os
 import scipy
-
+import matplotlib.pyplot as plt
 from curve import fit_quadratic_drag
 from cvat_annotations import load_track
 
@@ -11,24 +11,41 @@ def test_dataset(root):
     total_dist = 0
     total_tasks = 0
     for task in sorted(os.listdir(root)):
+        if total_tasks > 3:
+            break
         task_path = os.path.join(root, task)
         if not os.path.isdir(task_path):
             continue
         ann_path = os.path.join(task_path, 'annotations.json')
         track = load_track(ann_path)
-        track_times = np.arange(0, (len(track)) / 30, 1/30)
+        track_times = np.arange(0, (len(track)) / 30, 1 / 30)
         try:
             print("---- {} ----".format(task))
-            result = fit_quadratic_drag(track[0:2], track_times)
+            result = fit_quadratic_drag(track[0:3], track_times)
             dists = scipy.spatial.distance.cdist(result[:, [0, 1]], track)
             dist = sum(np.diagonal(dists)) / len(dists)
             total_dist += dist
             total_tasks += 1
             print(dist)
+            plot(result, track)
         except RuntimeError:
             print("Could not fit the curve for {}".format(task_path))
 
     print("total distance: {}".format(total_dist / total_tasks))
+
+
+def plot(result, track):
+    track = np.array(track)
+    track_times = np.arange(0, (len(track)) / 30, 1 / 30)
+    fig, axs = plt.subplots(1)
+    fig.suptitle('3d and camera projection')
+
+    axs.set(xlabel='ksi', ylabel='eta')
+    axs.plot(
+        track[:, 0], track[:, 1], '.',
+        result[:, 0], result[:, 1], '-'
+    )
+    plt.show()
 
 
 if __name__ == '__main__':
